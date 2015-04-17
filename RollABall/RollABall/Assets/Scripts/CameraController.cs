30db5bde43d958bbdb7ae8a50f19b7c0387570fe
David Basarab <dbasarab617@gmail.com>
2015-04-17 10:49:25 -0400

    RollABall Added

---
+++ RollABall/RollABall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Vector3 offset;
+        public GameObject player;
+
+        public void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+        }
+
+        public void Start()
+        {
+            offset = transform.position;
+        }
+    }
+}
---
+++ RollABall/RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private int _count;
+        public float speed;
+
+        public void FixedUpdate()
+        {
+            MovePlayer();
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (!IsPickUpObject(other)) return;
+
+            other.gameObject.SetActive(false);
+
+            _count++;
+        }
+
+        public void Start()
+        {
+            _count = 0;
+        }
+
+        private static bool IsPickUpObject(Collider other)
+        {
+            return other.gameObject.tag == "PickUp";
+        }
+
+        private void MovePlayer()
+        {
+            var moveHorizontal = Input.GetAxis("Horizontal");
+            var moveVertical = Input.GetAxis("Vertical");
+
+            var rigidBody = GetComponent<Rigidbody>();
+
+            var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            rigidBody.AddForce(movement * speed * Time.deltaTime);
+        }
+    }
+}
